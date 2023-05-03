8db3e7b2dea8255bc810e479dfc3259e55580e02
nickerions <131619173+nickerions@users.noreply.github.com>
2023-05-04 00:12:22 +0300

    Update trigger.ts

--- src/trigger.ts
+++ src/trigger.ts
@@ -1,5 +1,5 @@
 setInterval(async () => {
-  const res = await fetch("https://github.com/nickerions/notioneo-auto-month-script/blob/main/src/main.ts");
+  const res = await fetch("./src/main.ts");
   const script = await res.text();
   eval(script);
 }, 5000);

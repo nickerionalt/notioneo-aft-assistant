a1cbd59e7e09b6f35b6c831555b84a48f32dc661
nickerions <131619173+nickerions@users.noreply.github.com>
2023-05-07 15:37:01 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,12 @@ import { Client } from "https://deno.land/x/notion_sdk@v1.0.4/src/mod.ts";
 
 // Initialize a new Notion API client
 const notion = new Client({
-  auth: "secret_1znQ6olMQsv4LAUmiDUWxrZlAJ2hibu5Rut4LOVU0VB",
+  auth: Deno.env.get("NOTION_TOKEN"),
 });
-
+const NOTION_TOKEN = Deno.env.get("NOTION_TOKEN") as string
 // Set the IDs of the two databases
-const database1Id = '3a8fae42-9cc4-40b2-a322-724a4fe34918';
-const database2Id = '30238b71ba5246fdb159ca24d25b0414';
+const database1Id = Deno.env.get("DATABASE_1");
+const database2Id = Deno.env.get("DATABASE_2");
 
 // Set up a filter for database_1 to find RT Income and RT Expense items with empty Month relation
 const database1Filter = {
@@ -45,26 +45,26 @@ async function updateMonthPropertyIfEmpty(database1ItemId, database2ItemId) {
         },
       },
     });
-    console.log(`Updated Month property for database_1 item ${database1ItemId}`);
+    console.log(`Updated Month property for "Transactions" DB item ${database1ItemId}`);
   }
 }
 
 // Define the main function to watch database_1 and update the Month property when needed
 async function watchDatabase1() {
-  console.log('Watching database_1...');
+  console.log('Watching "Transactions" DB Database...');
 
   const response = await notion.databases.query({
     database_id: database1Id,
     filter: database1Filter,
   });
 
-  console.log(`Found ${response.results.length} RT Income/Expense items in database_1.`);
+  console.log(`Found ${response.results.length} items in "Transactions" DB.`);
 
   for (const database1Item of response.results) {
     // Get the Month Text formula from the database_1 item
     const monthTextFormula = database1Item.properties['Month Text'].formula.string;
 
-    console.log(`Checking for matching Month in database_2 for database_1 item ${database1Item.id}...`);
+    console.log(`Checking for matching Month in "Months" DB for "Transactions" DB item ${database1Item.id}...`);
 
     // Update the filter for database_2 to use the Month Text formula from database_1
     database2Filter.formula.string.equals = monthTextFormula;
@@ -78,9 +78,9 @@ async function watchDatabase1() {
     if (response2.results.length > 0) {
       // Update the Month property in database_1 with the first matching database_2 item if it is empty
       await updateMonthPropertyIfEmpty(database1Item.id, response2.results[0].id);
-      console.log(`Linked database_1 item ${database1Item.id} with database_2 item ${response2.results[0].id}.`);
+      console.log(`Linked "Transactions" DB item ${database1Item.id} with "Month" DB item ${response2.results[0].id}.`);
     } else {
-      console.log(`No matching Month found in database_2 for database_1 item ${database1Item.id}.`);
+      console.log(`No matching Month found in "Month" DB for "Transactions" DB item ${database1Item.id}.`);
     }
   }
 

8493bf0f6e2716a72252f6395cf53b9103c82967
nickerions <131619173+nickerions@users.noreply.github.com>
2023-05-07 17:50:08 +0300

    Delete trigger.ts

--- src/trigger.ts
+++
@@ -1,9 +0,0 @@
-setInterval(async () => {
-  const res = await fetch("https://raw.githubusercontent.com/nickerions/notioneo-auto-month-script/main/src/main.ts");
-  const script = await res.text();
-  try {
-    eval(script);
-  } catch (error) {
-    console.error(error);
-  }
-}, 5000);

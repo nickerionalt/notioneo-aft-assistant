f5b409bac186539a026354550c44e78bd7eb1e6c
nickerions <131619173+nickerions@users.noreply.github.com>
2023-05-18 03:38:59 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -28,7 +28,7 @@ async function getDatabaseId(database: DatabaseType): Promise<string> {
   const databaseId = results
     .filter(r =>
       r?.type === "child_database" &&
-      r.child_database?.title.toLowerCase() === database.toLowerCase()
+      r.child_database?.title[0]?.plain_text.toLowerCase() === database.toLowerCase()
     )
     ?.map(r => r.id)[0] || '';
 

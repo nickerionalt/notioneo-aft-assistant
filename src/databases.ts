9799affdd7f17643f8a550a9d6fab66d6bc5cad1
nickerions <131619173+nickerions@users.noreply.github.com>
2023-05-18 03:52:34 +0300

    Update databases.ts

--- src/databases.ts
+++ src/databases.ts
@@ -1,6 +1,18 @@
 import { BlockObjectResponse, CreatePageParameters, QueryDatabaseResponse } from "https://deno.land/x/notion_sdk@v1.0.4/src/api-endpoints.ts";
 import { Client } from "https://deno.land/x/notion_sdk@v1.0.4/src/mod.ts";
 
+const NOTION_TOKEN = Deno.env.get("NOTION_TOKEN");
+const DATABASE_PAGE = Deno.env.get("DATABASE_PAGE");
+
+if (!NOTION_TOKEN || !DATABASE_PAGE) {
+    throw new Error("Notion token or database IDs not found. Please, re-check the values again or write a support request to contact@notioneo.com.");
+}
+
+// Initialize a new Notion API client
+const notion = new Client({
+    auth: NOTION_TOKEN,
+});
+
 export enum DatabaseType {
     Transactions = "Transactions",
     Month = "Month",
